6bb31de37cd94e0aa8e62ddfa8be853994ccf9a2
denomelchenko <denysomelchenko.dev@gmail.com>
2026-09-21 13:11:36 +0200

    part6: exercise 6.19 extract the useAnecdotes hook

--- query-anecdotes/src/App.jsx
+++ query-anecdotes/src/App.jsx
@@ -1,42 +1,9 @@
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
-import anecdoteService from './services/anecdotes'
+import { useAnecdotes } from './hooks/useAnecdotes'
 
 const App = () => {
-  const queryClient = useQueryClient()
-
-  const {
-    isPending,
-    isError,
-    data: anecdotes,
-  } = useQuery({
-    queryKey: ['anecdotes'],
-    queryFn: anecdoteService.getAll,
-  })
-
-  const addAnecdoteMutation = useMutation({
-    mutationFn: anecdoteService.createNew,
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
-    },
-  })
-
-  const voteMutation = useMutation({
-    mutationFn: (anecdote) =>
-      anecdoteService.update({ ...anecdote, votes: anecdote.votes + 1 }),
-    onSuccess: (votedAnecdote) => {
-      queryClient.setQueryData(['anecdotes'], (anecdotes) =>
-        anecdotes.map((anecdote) =>
-          anecdote.id === votedAnecdote.id ? votedAnecdote : anecdote
-        )
-      )
-    },
-  })
-
-  const vote = (anecdote) => {
-    voteMutation.mutate(anecdote)
-  }
+  const { anecdotes, isPending, isError, addAnecdote, vote } = useAnecdotes()
 
   if (isError) {
     return <div>anecdote service not available due to problems in server</div>
@@ -47,7 +14,7 @@ const App = () => {
       <h3>Anecdote app</h3>
 
       <Notification />
-      <AnecdoteForm addAnecdote={addAnecdoteMutation.mutate} />
+      <AnecdoteForm addAnecdote={addAnecdote} />
 
       {isPending ? (
         <div>loading anecdotes...</div>
---
+++ query-anecdotes/src/hooks/useAnecdotes.js
@@ -0,0 +1,42 @@
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import anecdoteService from '../services/anecdotes'
+
+export const useAnecdotes = () => {
+  const queryClient = useQueryClient()
+
+  const {
+    isPending,
+    isError,
+    data: anecdotes,
+  } = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: anecdoteService.getAll,
+  })
+
+  const addAnecdoteMutation = useMutation({
+    mutationFn: anecdoteService.createNew,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+    },
+  })
+
+  const voteMutation = useMutation({
+    mutationFn: (anecdote) =>
+      anecdoteService.update({ ...anecdote, votes: anecdote.votes + 1 }),
+    onSuccess: (votedAnecdote) => {
+      queryClient.setQueryData(['anecdotes'], (anecdotes) =>
+        anecdotes.map((anecdote) =>
+          anecdote.id === votedAnecdote.id ? votedAnecdote : anecdote
+        )
+      )
+    },
+  })
+
+  return {
+    anecdotes,
+    isPending,
+    isError,
+    addAnecdote: addAnecdoteMutation.mutate,
+    vote: voteMutation.mutate,
+  }
+}
