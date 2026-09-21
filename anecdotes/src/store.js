456f00e9c3fb38691d197aab1ec54bf7dcb2c1c2
denomelchenko <denysomelchenko.dev@gmail.com>
2026-09-21 13:06:36 +0200

    part6: exercise 6.7 anecdotes step 6 fetch anecdotes with the fetch api

--- anecdotes/src/store.js
+++ anecdotes/src/store.js
@@ -1,26 +1,14 @@
 import { create } from 'zustand'
-
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
-const getId = () => (100000 * Math.random()).toFixed(0)
-
-const asObject = (anecdote) => ({
-  content: anecdote,
-  id: getId(),
-  votes: 0,
-})
+import anecdoteService from './services/anecdotes'
 
 export const useAnecdoteStore = create((set) => ({
-  anecdotes: anecdotesAtStart.map(asObject),
+  anecdotes: [],
   filter: '',
   actions: {
+    initialize: async () => {
+      const anecdotes = await anecdoteService.getAll()
+      set({ anecdotes })
+    },
     vote: (id) =>
       set((state) => ({
         anecdotes: state.anecdotes.map((anecdote) =>
@@ -31,7 +19,7 @@ export const useAnecdoteStore = create((set) => ({
       set((state) => ({
         anecdotes: state.anecdotes.concat({
           content,
-          id: getId(),
+          id: (100000 * Math.random()).toFixed(0),
           votes: 0,
         }),
       })),
@@ -43,10 +31,10 @@ export const useAnecdotes = () => useAnecdoteStore((state) => state.anecdotes)
 export const useFilter = () => useAnecdoteStore((state) => state.filter)
 export const useAnecdoteActions = () => useAnecdoteStore((state) => state.actions)
 
+export const sortByVotes = (anecdotes) =>
+  anecdotes.toSorted((a, b) => b.votes - a.votes)
+
 export const filterAnecdotes = (anecdotes, filter) =>
   anecdotes.filter((anecdote) =>
     anecdote.content.toLowerCase().includes(filter.toLowerCase())
   )
-
-export const sortByVotes = (anecdotes) =>
-  anecdotes.toSorted((a, b) => b.votes - a.votes)
